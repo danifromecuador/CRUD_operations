e0f73cdd87875520b8c52b2d25a38d8fa354d034
dani <danifromecuador@gmail.com>
2024-01-19 21:07:14 -0500

    Add new and create methods for create a new Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,21 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
-    puts '////////////////////'
-    pp @article
+    # puts '////////////////////'
+    # pp @article
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(title: "...", body: "...")
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 end
